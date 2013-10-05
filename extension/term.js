1121cc4c498151477abca75466e21dd94597a26f
Brian Chu <bc@brianchu.com>
2013-10-05 16:56:08 -0400

    Basic styles, socket interop

--- extension/term.js
+++ extension/term.js
@@ -1,18 +1,36 @@
+$ = function(sel) {
+  var self = this;
+  if (self === window) {
+    self = document;
+  }
+  if (sel[0] === '#') {
+    return self.getElementById(sel.slice(1));
+  }
+  else {
+    return self.querySelector(sel);
+  }
+};
+
+var socket = io.connect('http://localhost:4109');
+
+
 window.addEventListener('DOMContentLoaded', function() {
-  $ = function(sel) {
-    var self = this;
-    if (self === window) {
-      self = document;
-    }
-    if (sel[0] === '#') {
-      return self.getElementById(sel.slice(1));
-    }
-    else {
-      return self.querySelector(sel);
-    }
+  var term = $('.terminal');
+  term.find = $;
+
+  var input = term.find('input');
+  var prompt = term.find('.prompt');
+
+  var handler = function() {
+    input.addEventListener('input', function() {
+      socket.emit('input', input.value);
+    }, false);
   };
 
-  var term = $('.term');
-  term.find = $;
+  socket.on('connect', handler);
+
+  socket.on('output', function (data) {
+    console.log(data);
+  });
 
 }, false);
