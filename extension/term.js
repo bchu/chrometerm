1922f08b54d4a43841d7bfad9cdfb2f1d7dc370d
Brian Chu <bc@brianchu.com>
2013-10-05 23:16:48 -0400

    Major style changes, terminal evaluation loop, cd process forking

--- extension/term.js
+++ extension/term.js
@@ -14,23 +14,54 @@ $ = function(sel) {
 var socket = io.connect('http://localhost:4109');
 
 
+var line = function(text) {
+  var li = document.createElement('li');
+  li.textContent = text;
+  return li;
+};
+
 window.addEventListener('DOMContentLoaded', function() {
   var term = $('.terminal');
   term.find = $;
 
-  var input = term.find('input');
-  var prompt = term.find('.prompt');
+  var prompt = term.find('.input');
+  var input = term.find('textarea');
+  var sizeInput = term.find('.text-copy');
+
+  var outputHandler = function(data) {
+    if (data) {
+      var li = line(data);
+      term.insertBefore(li, prompt);
+    }
+  };
+  socket.on('stdout', outputHandler);
+  socket.on('stderr', outputHandler);
 
   var handler = function() {
-    input.addEventListener('input', function() {
-      socket.emit('input', input.value);
+    input.addEventListener('keydown', function(e) {
+      if (e.keyCode === 13) {
+        var cmd = input.value.replace('/\n/g', '');
+        socket.emit('input', cmd);
+        input.value = '';
+        autoSize();
+        outputHandler(cmd);
+      }
     }, false);
   };
 
   socket.on('connect', handler);
 
-  socket.on('output', function (data) {
-    console.log(data);
-  });
+
+  var autoSize = function autoSize() {
+    // Copy textarea contents; browser will calculate correct height of copy,
+    // which will make overall container taller, which will make textarea taller.
+    var text = input.value.replace(/\n/g, '<br/>');
+    sizeInput.innerHTML = text;
+  };
+  autoSize();
+
+  input.addEventListener('change',autoSize);
+  input.addEventListener('keydown',autoSize);
+  input.addEventListener('keyup',autoSize);
 
 }, false);
